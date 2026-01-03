e2b608432ba37a934e7330a3723af2c1082aa5ce
Aloys <jiangxinnju@163.com>
2026-01-04 00:03:21 +0800

    refactor code: move main window routing and menu wiring into internal/ui/app

--- cmd/game_tool_box/main.go
+++ cmd/game_tool_box/main.go
@@ -1,25 +1,12 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"net/url"
-	"strings"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/dialog"
-	"fyne.io/fyne/v2/theme"
 
-	"github.com/game_tool_box/internal/config"
 	"github.com/game_tool_box/internal/i18n"
 	"github.com/game_tool_box/internal/logging"
-	"github.com/game_tool_box/internal/resources"
-	aboutui "github.com/game_tool_box/internal/ui/about"
-	"github.com/game_tool_box/internal/ui/pegasus"
-	settingsui "github.com/game_tool_box/internal/ui/settings"
-	"github.com/game_tool_box/internal/update"
+	appui "github.com/game_tool_box/internal/ui/app"
 )
 
 func main() {
@@ -35,195 +22,9 @@ func main() {
 	fyneApp := app.New()
 	fyneWindow := fyneApp.NewWindow(i18n.T(i18n.Current(), "app.title"))
 
-	appTitle := func() string { return i18n.T(i18n.Current(), "app.title") }
-	setBreadcrumb := func(crumb string) {
-		crumb = strings.TrimSpace(crumb)
-		if crumb == "" {
-			fyneWindow.SetTitle(appTitle())
-			return
-		}
-		fyneWindow.SetTitle(appTitle() + " - " + crumb)
-	}
+	_ = appui.NewShell(fyneApp, fyneWindow)
 
-	if cfg, err := config.Load(); err == nil {
-		switch strings.ToLower(strings.TrimSpace(cfg.Theme)) {
-		case "light":
-			fyneApp.Settings().SetTheme(theme.LightTheme())
-		case "dark":
-			fyneApp.Settings().SetTheme(theme.DarkTheme())
-		default:
-			// "system" or empty => follow system preference.
-			fyneApp.Settings().SetTheme(theme.DefaultTheme())
-		}
-	}
-
-	fyneWindow.SetIcon(resources.IconPng)
-
-	// Default startup view: About page rendered from Markdown.
-	aboutMD := strings.ReplaceAll(resources.AboutMarkdown(), "{VERSION}", resources.Version)
-	mainView := aboutui.New(aboutMD)
-
-	router := container.NewStack(mainView)
-	fyneWindow.SetContent(router)
-
-	t := func(key string) string { return i18n.T(i18n.Current(), key) }
-
-	var rebuildMenu func()
-	var showSettings func()
-
-	rebuildMenu = func() {
-		// Keep current breadcrumb if any by default: menu rebuild updates app title only.
-		fyneWindow.SetTitle(appTitle())
-
-		mPegasus := fyne.NewMenu(t("menu.pegasus"))
-		mSettings := fyne.NewMenu(t("menu.settings"))
-		mHelp := fyne.NewMenu(t("menu.help"))
-
-		pegasusRomManager := fyne.NewMenuItem(t("menuitem.pegasus.romManager"), func() {
-			logging.Infof("menu click: pegasus.romManager")
-			setBreadcrumb(t("menu.pegasus") + " / " + t("menuitem.pegasus.romManager"))
-			view := pegasusui.NewRomeManagerView(fyneWindow)
-			router.Objects = []fyne.CanvasObject{view}
-			router.Refresh()
-		})
-		mPegasus.Items = append(mPegasus.Items, pegasusRomManager)
-
-		pegasusMediaManager := fyne.NewMenuItem(t("menuitem.pegasus.mediaManager"), func() {
-			logging.Infof("menu click: pegasus.mediaManager")
-			setBreadcrumb(t("menu.pegasus") + " / " + t("menuitem.pegasus.mediaManager"))
-			view := pegasusui.NewMediaManagerView(fyneWindow)
-			router.Objects = []fyne.CanvasObject{view}
-			router.Refresh()
-		})
-		mPegasus.Items = append(mPegasus.Items, pegasusMediaManager)
-
-		pegasusConfigManager := fyne.NewMenuItem(t("menuitem.pegasus.configManager"), func() {
-			logging.Infof("menu click: pegasus.configManager")
-			setBreadcrumb(t("menu.pegasus") + " / " + t("menuitem.pegasus.configManager"))
-			view := pegasusui.NewConfigManagerView(fyneWindow)
-			router.Objects = []fyne.CanvasObject{view}
-			router.Refresh()
-		})
-		mPegasus.Items = append(mPegasus.Items, pegasusConfigManager)
-
-		pegasusGameRemover := fyne.NewMenuItem(t("menuitem.pegasus.gameRemover"), func() {
-			logging.Infof("menu click: pegasus.gameRemover")
-			setBreadcrumb(t("menu.pegasus") + " / " + t("menuitem.pegasus.gameRemover"))
-			view := pegasusui.NewGameRemoverView(fyneWindow)
-			router.Objects = []fyne.CanvasObject{view}
-			router.Refresh()
-		})
-		mPegasus.Items = append(mPegasus.Items, pegasusGameRemover)
-
-		showSettings = func() {
-			logging.Infof("menu click: settings.settings")
-			setBreadcrumb(t("menu.settings") + " / " + t("menuitem.settings.settings"))
-			view := settingsui.NewSettingsView(
-				t,
-				func(newLang i18n.Lang) {
-					logging.Infof("settings change: lang=%s", newLang)
-					i18n.SetCurrentPersisted(newLang)
-				},
-				func() {
-					// Rebuild and re-set menus after theme/lang changes.
-					rebuildMenu()
-					setBreadcrumb(t("menu.settings") + " / " + t("menuitem.settings.settings"))
-				},
-			)
-			router.Objects = []fyne.CanvasObject{container.NewPadded(view)}
-			router.Refresh()
-		}
-		mSettings.Items = append(mSettings.Items, fyne.NewMenuItem(t("menuitem.settings.settings"), showSettings))
-
-		goDocs := fyne.NewMenuItem(t("menuitem.help.docs"), func() {
-			logging.Infof("menu click: help.docs")
-			u, err := url.Parse("https://jiangxincode.github.io/GameToolBox")
-			if err != nil {
-				dialog.ShowError(err, fyneWindow)
-				return
-			}
-			_ = fyneApp.OpenURL(u)
-		})
-		mHelp.Items = append(mHelp.Items, goDocs)
-		mHelp.Items = append(mHelp.Items, fyne.NewMenuItemSeparator())
-
-		feedback := fyne.NewMenuItem(t("menuitem.help.feedback"), func() {
-			logging.Infof("menu click: help.feedback")
-			u, err := url.Parse("https://github.com/jiangxincode/GameToolBox/issues/new")
-			if err != nil {
-				dialog.ShowError(err, fyneWindow)
-				return
-			}
-			_ = fyneApp.OpenURL(u)
-		})
-		mHelp.Items = append(mHelp.Items, feedback)
-
-		checkUpdate := func() {
-			logging.Infof("menu click: help.update")
-			progress := dialog.NewProgress(t("menuitem.help.update"), "...", fyneWindow)
-			progress.Show()
-			progressInfinite := progress
-			progressInfinite.SetValue(-1)
-
-			go func() {
-				info, err := update.LatestRelease(context.Background(), "jiangxincode", "GameToolBox")
-
-				current := strings.TrimSpace(resources.Version)
-
-				var msg string
-				if err == nil {
-					msg = fmt.Sprintf("Current: %s\nLatest:  %s", current, info.TagName)
-					if info.HTMLURL != "" {
-						msg += "\n\n" + info.HTMLURL
-					}
-				}
-
-				fyne.Do(func() {
-					progress.Hide()
-					if err != nil {
-						dialog.ShowError(err, fyneWindow)
-						return
-					}
-					dialog.ShowInformation(t("menuitem.help.update"), msg, fyneWindow)
-				})
-			}()
-		}
-		updateItem := fyne.NewMenuItem(t("menuitem.help.update"), checkUpdate)
-		mHelp.Items = append(mHelp.Items, updateItem)
-
-		contrib := fyne.NewMenuItem(t("menuitem.help.contrib"), func() {
-			logging.Infof("menu click: help.contrib")
-			u, err := url.Parse("https://github.com/jiangxincode/GameToolBox")
-			if err != nil {
-				dialog.ShowError(err, fyneWindow)
-				return
-			}
-			_ = fyneApp.OpenURL(u)
-		})
-		mHelp.Items = append(mHelp.Items, contrib)
-		mHelp.Items = append(mHelp.Items, fyne.NewMenuItemSeparator())
-
-		showAbout := func() {
-			logging.Infof("menu click: help.about")
-			setBreadcrumb(t("menu.help") + " / " + t("menuitem.help.about"))
-			router.Objects = []fyne.CanvasObject{mainView}
-			router.Refresh()
-		}
-		about := fyne.NewMenuItem(t("menuitem.help.about"), showAbout)
-		mHelp.Items = append(mHelp.Items, about)
-
-		// IMPORTANT: Set the main menu only after items are fully populated.
-		fyneWindow.SetMainMenu(fyne.NewMainMenu(mSettings, mPegasus, mHelp))
-	}
-
-	rebuildMenu()
-	setBreadcrumb(t("menu.help") + " / " + t("menuitem.help.about"))
-
-	resizeAndCenter := func(size fyne.Size) {
-		fyneWindow.Resize(size)
-		fyneWindow.CenterOnScreen()
-	}
-
-	resizeAndCenter(fyne.NewSize(900, 650))
+	fyneWindow.Resize(fyne.NewSize(900, 650))
+	fyneWindow.CenterOnScreen()
 	fyneWindow.ShowAndRun()
 }
---
+++ internal/ui/app/app.go
@@ -0,0 +1,233 @@
+package appui
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"strings"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/theme"
+
+	"github.com/game_tool_box/internal/config"
+	"github.com/game_tool_box/internal/i18n"
+	"github.com/game_tool_box/internal/logging"
+	"github.com/game_tool_box/internal/resources"
+	aboutui "github.com/game_tool_box/internal/ui/about"
+	pegasusui "github.com/game_tool_box/internal/ui/pegasus"
+	settingsui "github.com/game_tool_box/internal/ui/settings"
+	"github.com/game_tool_box/internal/update"
+)
+
+// Shell wires up the main window content router and menus.
+//
+// Contract:
+//   - NewShell does not call ShowAndRun; caller owns app lifecycle.
+//   - Shell methods are expected to be called from the main UI thread.
+type Shell struct {
+	app fyne.App
+	w   fyne.Window
+
+	router   *fyne.Container
+	mainView fyne.CanvasObject
+
+	t func(key string) string
+}
+
+func NewShell(app fyne.App, w fyne.Window) *Shell {
+	// Default startup view: About page rendered from Markdown.
+	aboutMD := strings.ReplaceAll(resources.AboutMarkdown(), "{VERSION}", resources.Version)
+	mainView := aboutui.New(aboutMD)
+
+	router := container.NewStack(mainView)
+	w.SetContent(router)
+	w.SetIcon(resources.IconPng)
+
+	s := &Shell{
+		app:      app,
+		w:        w,
+		router:   router,
+		mainView: mainView,
+		t: func(key string) string {
+			return i18n.T(i18n.Current(), key)
+		},
+	}
+
+	s.applyPersistedTheme()
+	s.rebuildMenu()
+	s.setBreadcrumb(s.t("menu.help") + " / " + s.t("menuitem.help.about"))
+
+	return s
+}
+
+func (s *Shell) applyPersistedTheme() {
+	if cfg, err := config.Load(); err == nil {
+		switch strings.ToLower(strings.TrimSpace(cfg.Theme)) {
+		case "light":
+			s.app.Settings().SetTheme(theme.LightTheme())
+		case "dark":
+			s.app.Settings().SetTheme(theme.DarkTheme())
+		default:
+			// "system" or empty => follow system preference.
+			s.app.Settings().SetTheme(theme.DefaultTheme())
+		}
+	}
+}
+
+func (s *Shell) appTitle() string {
+	return s.t("app.title")
+}
+
+func (s *Shell) setBreadcrumb(crumb string) {
+	crumb = strings.TrimSpace(crumb)
+	if crumb == "" {
+		s.w.SetTitle(s.appTitle())
+		return
+	}
+	s.w.SetTitle(s.appTitle() + " - " + crumb)
+}
+
+func (s *Shell) navigate(crumb string, view fyne.CanvasObject) {
+	s.setBreadcrumb(crumb)
+	s.router.Objects = []fyne.CanvasObject{view}
+	s.router.Refresh()
+}
+
+func (s *Shell) openURL(raw string) {
+	u, err := url.Parse(raw)
+	if err != nil {
+		dialog.ShowError(err, s.w)
+		return
+	}
+	_ = s.app.OpenURL(u)
+}
+
+func (s *Shell) rebuildMenu() {
+	// Reset to base title when rebuilding menus; navigation will set breadcrumb.
+	s.w.SetTitle(s.appTitle())
+
+	mPegasus := s.buildPegasusMenu()
+	mSettings := s.buildSettingsMenu()
+	mHelp := s.buildHelpMenu()
+
+	// IMPORTANT: Set the main menu only after items are fully populated.
+	s.w.SetMainMenu(fyne.NewMainMenu(mSettings, mPegasus, mHelp))
+}
+
+func (s *Shell) buildPegasusMenu() *fyne.Menu {
+	m := fyne.NewMenu(s.t("menu.pegasus"))
+	m.Items = append(m.Items,
+		fyne.NewMenuItem(s.t("menuitem.pegasus.romManager"), func() {
+			logging.Infof("menu click: pegasus.romManager")
+			view := pegasusui.NewRomeManagerView(s.w)
+			s.navigate(s.t("menu.pegasus")+" / "+s.t("menuitem.pegasus.romManager"), view)
+		}),
+		fyne.NewMenuItem(s.t("menuitem.pegasus.mediaManager"), func() {
+			logging.Infof("menu click: pegasus.mediaManager")
+			view := pegasusui.NewMediaManagerView(s.w)
+			s.navigate(s.t("menu.pegasus")+" / "+s.t("menuitem.pegasus.mediaManager"), view)
+		}),
+		fyne.NewMenuItem(s.t("menuitem.pegasus.configManager"), func() {
+			logging.Infof("menu click: pegasus.configManager")
+			view := pegasusui.NewConfigManagerView(s.w)
+			s.navigate(s.t("menu.pegasus")+" / "+s.t("menuitem.pegasus.configManager"), view)
+		}),
+		fyne.NewMenuItem(s.t("menuitem.pegasus.gameRemover"), func() {
+			logging.Infof("menu click: pegasus.gameRemover")
+			view := pegasusui.NewGameRemoverView(s.w)
+			s.navigate(s.t("menu.pegasus")+" / "+s.t("menuitem.pegasus.gameRemover"), view)
+		}),
+	)
+	return m
+}
+
+func (s *Shell) buildSettingsMenu() *fyne.Menu {
+	m := fyne.NewMenu(s.t("menu.settings"))
+
+	showSettings := func() {
+		logging.Infof("menu click: settings.settings")
+		view := settingsui.NewSettingsView(
+			s.t,
+			func(newLang i18n.Lang) {
+				logging.Infof("settings change: lang=%s", newLang)
+				i18n.SetCurrentPersisted(newLang)
+			},
+			func() {
+				// Rebuild and re-set menus after theme/lang changes.
+				s.rebuildMenu()
+				// Keep user on settings page.
+				s.setBreadcrumb(s.t("menu.settings") + " / " + s.t("menuitem.settings.settings"))
+			},
+		)
+
+		s.navigate(s.t("menu.settings")+" / "+s.t("menuitem.settings.settings"), container.NewPadded(view))
+	}
+
+	m.Items = append(m.Items, fyne.NewMenuItem(s.t("menuitem.settings.settings"), showSettings))
+	return m
+}
+
+func (s *Shell) buildHelpMenu() *fyne.Menu {
+	m := fyne.NewMenu(s.t("menu.help"))
+
+	m.Items = append(m.Items,
+		fyne.NewMenuItem(s.t("menuitem.help.docs"), func() {
+			logging.Infof("menu click: help.docs")
+			s.openURL("https://jiangxincode.github.io/GameToolBox")
+		}),
+		fyne.NewMenuItemSeparator(),
+		fyne.NewMenuItem(s.t("menuitem.help.feedback"), func() {
+			logging.Infof("menu click: help.feedback")
+			s.openURL("https://github.com/jiangxincode/GameToolBox/issues/new")
+		}),
+	)
+
+	m.Items = append(m.Items, fyne.NewMenuItem(s.t("menuitem.help.update"), s.menuCheckUpdate()))
+
+	m.Items = append(m.Items,
+		fyne.NewMenuItem(s.t("menuitem.help.contrib"), func() {
+			logging.Infof("menu click: help.contrib")
+			s.openURL("https://github.com/jiangxincode/GameToolBox")
+		}),
+		fyne.NewMenuItemSeparator(),
+		fyne.NewMenuItem(s.t("menuitem.help.about"), func() {
+			logging.Infof("menu click: help.about")
+			s.navigate(s.t("menu.help")+" / "+s.t("menuitem.help.about"), s.mainView)
+		}),
+	)
+
+	return m
+}
+
+func (s *Shell) menuCheckUpdate() func() {
+	return func() {
+		logging.Infof("menu click: help.update")
+		progress := dialog.NewProgress(s.t("menuitem.help.update"), "...", s.w)
+		progress.Show()
+		progress.SetValue(-1)
+
+		go func() {
+			info, err := update.LatestRelease(context.Background(), "jiangxincode", "GameToolBox")
+			current := strings.TrimSpace(resources.Version)
+
+			var msg string
+			if err == nil {
+				msg = fmt.Sprintf("Current: %s\nLatest:  %s", current, info.TagName)
+				if info.HTMLURL != "" {
+					msg += "\n\n" + info.HTMLURL
+				}
+			}
+
+			fyne.Do(func() {
+				progress.Hide()
+				if err != nil {
+					dialog.ShowError(err, s.w)
+					return
+				}
+				dialog.ShowInformation(s.t("menuitem.help.update"), msg, s.w)
+			})
+		}()
+	}
+}
